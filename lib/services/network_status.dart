6c1e0911e60ed265524af66448b2a3bb0384cea8
augustinewafula <augustinetreezy@outlook.com>
2022-09-06 18:03:47 +0300

    create NetworkStatus class

---
+++ lib/services/network_status.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+class NetworkStatus {
+  Future<bool> isInternetAvailable() async {
+    try {
+      final result = await InternetAddress.lookup('example.com');
+      return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
+    } on SocketException catch (_) {
+      return false;
+    }
+  }
+}
